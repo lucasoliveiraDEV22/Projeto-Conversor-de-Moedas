75d55fabb70848f23ddb88b02a44df07938620f1
lucasoliveiraDEV22 <lucaszrones@gmail.com>
2022-12-20 17:34:39 -0300

    Projeto-Conversor-Lucas-+-Bitcoin-Desafio

---
+++ script.js
@@ -0,0 +1,62 @@
+const button = document.getElementById("converter-Button")
+const select = document.getElementById ("select-currency")
+//console.log (button)
+const dolar = 5.2
+const euro = 5.5
+const bitcoin = 87468.5
+const convertValues = () => {
+    const inputReais = document.getElementById("input-real").value
+    const realValueText = document.getElementById("real-value-text")
+    const currencyValueText = document.getElementById("currency-value-text")
+    realValueText.innerHTML = new Intl.NumberFormat('pt-BR', {
+        style: 'currency', 
+        currency: 'BRL'
+
+    }).format(inputReais)
+    if (select.value === 'US$ Dólar Americano') {
+    currencyValueText.innerHTML = new Intl.NumberFormat('en-US', {
+        style: 'currency', 
+        currency: 'USD'
+    }).format(inputReais/dolar)
+}
+
+if (select.value === '€ Euro') {
+    currencyValueText.innerHTML = new Intl.NumberFormat('de-DE', { 
+        style: 'currency', 
+        currency: 'EUR' 
+    }).format(inputReais/euro)
+}
+if (select.value === 'Bitcoin')
+    currencyValueText.innerHTML = new Intl.NumberFormat('ja-JP', { 
+        style: 'currency', 
+        currency: 'BTC' 
+    }).format(inputReais/bitcoin)
+    //realValueText.innerHTML = inputReais
+    //currencyValueText.innerHTML = inputReais / dolar
+    // console.log (inputReais)
+    // console.log(inputReais/dolar)
+    //console.log ('cliquei no botão')
+}
+const changeCurrency = () =>{
+    const currencyName = document.getElementById("text-currency")
+    const currencyImg = document.getElementById ("flag")
+    if (select.value === 'US$ Dólar Americano'){
+        currencyName.innerHTML = "Dólar Americano"
+        currencyImg.src = "./assets/assets/Estados Unidos.png"
+    }
+    if (select.value === '€ Euro'){
+        currencyName.innerHTML = "Euro"
+        currencyImg.src = "./assets/assets/Euro.png"
+    }
+
+    if (select.value === 'Bitcoin'){
+        currencyName.innerHTML = "Bitcoin"
+        currencyImg.src = "./assets/assets/Bitcoin.png"
+    }
+
+convertValues ()
+    //console.log (select.value)
+    // console.log ("fui chamada")
+}
+button.addEventListener('click', convertValues)
+select.addEventListener ('change', changeCurrency)
