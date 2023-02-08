a4deca1e51c0d17e3c868cd183186a4ae64afc6f
lucasoliveiraDEV22 <lucaszrones@gmail.com>
2023-02-08 15:43:54 -0300

    Saved

--- script.js
+++ script.js
@@ -8,11 +8,13 @@ const convertValues = async() => {
     const inputReais = document.getElementById("input-real").value
     const realValueText = document.getElementById("real-value-text")
     const currencyValueText = document.getElementById("currency-value-text")
+
     const data = await fetch ("http://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,BTC-BRL").then(response => response.json())
     const dolar = data.USDBRL.high
     const euro = data.EURBRL.high
     const bitcoin = data.BTCBRL.high
     console.log (data)
+    
     realValueText.innerHTML = new Intl.NumberFormat('pt-BR', {
         style: 'currency', 
         currency: 'BRL'
